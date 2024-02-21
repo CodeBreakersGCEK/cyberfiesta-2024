0ad3938453963d8852a0e1a742acee72d9f0221b
Harsh Keshri <harshkeshri1234567@gmail.com>
2024-02-22 04:05:15 +0530

    Updated Easter Egg

--- easter-egg/src/App.tsx
+++ easter-egg/src/App.tsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+
+  useEffect(() => {
+    const audioElement = audioRef.current
+
+    if (audioElement) {
+      // Start playing the video
+      audioElement.play()
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-full h-full object-cover">
+      <video className="w-full h-full object-cover" autoPlay loop muted>
+        <source
+          src="/roll.mp4"
+          type="video/mp4"
+          className="w-full h-full object-cover"
+        />
+      </video>
+      <audio autoPlay loop ref={audioRef}>
+        <source src="/roll.mp3" type="audio/mpeg" />
+      </audio>
+    </div>
   )
 }
 
